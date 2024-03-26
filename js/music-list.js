68a2c3f9915075d747937bbd2a7ea134626330dc
Gautham Bhandary <gauthambhandary05@gmail.com>
2024-03-26 09:47:50 +0530

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,50 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Hymn For The Weekend",
+    artist: "Coldplay",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Shawn Mendes, Camila Cabello",
+    artist: "Señorita",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "On My Way",
+    artist: "Alan Walker",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Shape of you",
+    artist: "Ed Sheeran",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "FRIENDS",
+    artist: "Marshmello & Anne-Marie",
+    img: "music-11",
+    src: "music-11"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
